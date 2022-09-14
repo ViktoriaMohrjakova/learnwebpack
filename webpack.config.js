4af70e3cc27ca5562dfe1d1cd082a8f9add41ce1
Tehnikumi Opilane <opilane@tptlive.ee>
2022-09-14 13:57:29 +0300

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const glob = require('glob');
+
+let htmlPlugins = [];
+
+let files = glob.sync('./src/views/*.twig');
+files.forEach(file => {
+  let htmlPlugin = new HtmlWebpackPlugin({
+    filename: file.split('/').at(-1).replace('twig', 'html'),
+    template: file
+  });
+  htmlPlugins.push(htmlPlugin);
+});
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.twig$/,
+        use: {
+          loader: 'twig-loader',
+          options: {
+              // See options section below
+          },
+        }
+      }
+    ],
+  },
+  plugins: [
+      ...htmlPlugins,
+    new MiniCssExtractPlugin()
+        
+    
+  ],
+};
